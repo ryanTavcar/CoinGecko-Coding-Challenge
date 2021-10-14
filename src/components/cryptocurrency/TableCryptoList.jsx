dfb84fc7a1da2bb7666c8aa794b9295af694a57b
[Ryan Tavcar] <[Ryan.Tavcar@gmail.com]>
2021-10-14 18:50:29 +1100

    fixed bug fetching news

--- src/components/cryptocurrency/TableCryptoList.jsx
+++ src/components/cryptocurrency/TableCryptoList.jsx
@@ -22,7 +22,7 @@ const TableCryptoList = (props) => {
 
     const classes = useTableStyles()
     const [currentPage, setCurrentPage] = useState(1)
-    console.log(data)
+
     const currentTableData = useMemo(() => {
         const firstPageIndex = (currentPage - 1) * pageSize
         const lastPageIndex = firstPageIndex + pageSize
